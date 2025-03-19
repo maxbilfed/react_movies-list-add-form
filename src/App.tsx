e2067b4c4d03f87eb1e6c7b1fc0d0f4f5affa16c
Max Shapes <makc.bilous04@gmail.com>
2025-03-19 17:04:20 +0200

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
+import { Movie } from './types/Movie';
+import { useState } from 'react';
+
 export const App = () => {
+  const [allMovies, setAllMovies] = useState(moviesFromServer);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={allMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie
+          onAdd={(movie: Movie) => {
+            setAllMovies([...allMovies, movie]);
+          }}
+        />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,118 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie = ({ onAdd }: Props) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [titleText, setTitleText] = useState('');
+  const [descriptionText, setDescriptionText] = useState('');
+  const [imgUrlText, setImgUrlText] = useState('');
+  const [imdbUrlText, setImdbUrlText] = useState('');
+  const [imdbIdText, setImdbIdText] = useState('');
+
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+
+  function updateDisableSubmitState() {
+    if (titleText && imgUrlText && imdbIdText && imdbUrlText) {
+      setIsSubmitDisabled(false);
+    } else {
+      setIsSubmitDisabled(true);
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    updateDisableSubmitState();
+
+    if (isSubmitDisabled) {
+      return;
+    }
+
+    setCount(count + 1);
+
+    onAdd({
+      title: titleText,
+      description: descriptionText,
+      imgUrl: imgUrlText,
+      imdbUrl: imdbUrlText,
+      imdbId: imdbIdText,
+    });
+
+    setTitleText('');
+    setDescriptionText('');
+    setImgUrlText('');
+    setImdbUrlText('');
+    setImdbIdText('');
+
+    setIsSubmitDisabled(true);
+  }
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={titleText}
+        onChange={text => {
+          setTitleText(text);
+          updateDisableSubmitState();
+        }}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={descriptionText}
+        onChange={text => {
+          setDescriptionText(text);
+          updateDisableSubmitState();
+        }}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrlText}
+        onChange={text => {
+          setImgUrlText(text);
+          updateDisableSubmitState();
+        }}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrlText}
+        onChange={text => {
+          setImdbUrlText(text);
+          updateDisableSubmitState();
+        }}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbIdText}
+        onChange={text => {
+          setImdbIdText(text);
+          updateDisableSubmitState();
+        }}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +120,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isSubmitDisabled}
           >
             Add
           </button>
